0093712c9e2f69df7c6868f880f2f2fed24c4b39
Valerio Varricchio <v.varricchio@gmail.com>
2014-02-12 18:25:40 +0100

    Moving planner_msgs into drc_shared...
    ...so that pilot_interface is independent on planner

---
+++ src/planner_msgs/flat_walk_cmd.cpp
@@ -0,0 +1,111 @@
+/* 
+ *  YARP Implementation of a flat_walk_command message 
+ *  acoording to the agreed interfaces. 
+ *
+ *  email: v.varricchio@gmail.com 
+ */
+
+#include <shared/planner_msgs/flat_walk_cmd.h>
+#include <assert.h>
+#include <math.h>
+#include <string.h>
+
+std::map<std::string,std::string> flat_walk_cmd::opposite = {
+  {"fwd", "bwd"},
+  {"bwd", "fwd"},
+  {"rotl", "rotr"},
+  {"rotr", "rotl"}
+};
+
+std::map<std::string,double> flat_walk_cmd::el = {
+  {"fwd", 0.1},
+  {"bwd", 0.1},
+  {"rotl", 1.5},
+  {"rotr", 1.5}
+};
+
+yarp::os::ConstString flat_walk_cmd::getTypeName() const {
+  return "flat_walk_cmd";
+}
+
+
+flat_walk_cmd::flat_walk_cmd(){}
+
+bool flat_walk_cmd::read(yarp::os::ConnectionReader& connection) {
+  action = connection.expectText();
+  amount = connection.expectDouble();
+  seq_num = connection.expectInt();
+  
+  return !connection.isError();
+}
+
+bool flat_walk_cmd::write(yarp::os::ConnectionWriter& connection) {
+  connection.appendString(action.c_str());
+  connection.appendDouble(amount);
+  connection.appendInt(seq_num);
+  
+  return !connection.isError();
+}
+
+bool flat_walk_cmd::add(const flat_walk_cmd& cmd){
+  // Returns false if commands cannot be added
+  cmd_struct cmds0 = this->to_struct();
+  cmd_struct cmds1 = cmd.to_struct();
+  cmd_struct res;
+  res.seq_num = seq_num;
+  res.turn_deg = cmds0.turn_deg +cmds1.turn_deg;
+  res.walk_meters = cmds0.walk_meters+cmds1.walk_meters;
+  
+  return from_struct(res);
+}
+
+void flat_walk_cmd::normalize(){
+  
+  if(amount<0){
+    amount = -amount;
+    action = opposite[action];
+  }
+}
+
+cmd_struct flat_walk_cmd::to_struct() const{
+  cmd_struct cmd_transl;
+  cmd_transl.seq_num = seq_num;
+  if(strcmp(action.c_str(), "fwd")==0){
+    cmd_transl.walk_meters = amount;
+    cmd_transl.turn_deg = 0;
+  }else if(strcmp(action.c_str(), "bwd")==0){
+    cmd_transl.walk_meters = -amount;
+    cmd_transl.turn_deg = 0;
+  }else if(strcmp(action.c_str(), "rotl")==0){
+    cmd_transl.walk_meters = 0;
+    cmd_transl.turn_deg = amount;
+  }else if(strcmp(action.c_str(), "rotr")==0){
+    cmd_transl.walk_meters = 0;
+    cmd_transl.turn_deg = -amount;	
+  }else{
+    std::cout << "Action '" << action << "' is either not valid or not implemented." << std::endl;
+    assert(false);
+  }
+  return cmd_transl;
+}
+
+bool flat_walk_cmd::from_struct(const cmd_struct& cmds){
+  seq_num = cmds.seq_num;
+  if(cmds.walk_meters*cmds.turn_deg != 0)
+    return false;
+  if(cmds.walk_meters!=0){
+    amount = fabs(cmds.walk_meters);
+    action = (cmds.walk_meters>0) ? "fwd" : "bwd";
+  }else if(cmds.turn_deg!=0){
+    amount = fabs(cmds.turn_deg);
+    action = (cmds.walk_meters>0) ? "rotl" : "rotr";
+  }
+}  
+
+std::pair<flat_walk_cmd, size_t> flat_walk_cmd::split(const flat_walk_cmd& cmd){
+  flat_walk_cmd cmd_el;
+  cmd_el.action = cmd.action;
+  size_t num = cmd.amount/el[cmd.action];
+  cmd_el.amount = cmd.amount/num; 
+  return std::make_pair(cmd_el, num);
+}
---
+++ src/planner_msgs/flat_walk_plan.cpp
@@ -0,0 +1,132 @@
+/* 
+ *  YARP Implementation of a flat_walk_plan message as a sequence of flat_walk_cmd
+ *  with a few conversion utilities. 
+ *
+ *  email: v.varricchio@gmail.com 
+ */
+
+#include <shared/planner_msgs/flat_walk_plan.h>
+#include <shared/yarp_msgs/YARP_Pose.h>
+#include <tuple>
+
+flat_walk_plan::flat_walk_plan(){
+};
+
+yarp::os::ConstString flat_walk_plan::getTypeName() const {
+  return "flat_walk_plan";
+}
+
+bool flat_walk_plan::read(yarp::os::ConnectionReader& connection) {
+  controls.clear();
+  status = connection.expectInt();
+  progress = connection.expectInt(); 
+  int len = connection.expectInt();
+  
+  for (uint i=0; i<len; i++){
+    flat_walk_cmd p; 
+    p.read(connection);
+    controls.push_back(p);
+  }
+  
+  init_pose.read(connection);
+  
+  return !connection.isError();
+}
+
+bool flat_walk_plan::write(yarp::os::ConnectionWriter& connection) {
+  connection.appendInt(status);
+  connection.appendInt(progress);
+  connection.appendInt(controls.size());
+  
+  for(flat_walk_cmd cmd: controls){
+    cmd.write(connection);
+  }
+  
+  init_pose.write(connection);
+  
+  return !connection.isError();
+}
+
+std::vector<YARP_Point> flat_walk_plan::to_path(){
+  std::vector<YARP_Point> res;
+  res.push_back(init_pose);
+  YARP_Point p;
+  Pose2D next;
+  Pose2D cur(init_pose.x, init_pose.y, init_pose.z);
+  for(flat_walk_cmd cmd: controls){
+    next = next_pose(cur, cmd);
+    p.x = next.x;
+    p.y = next.y;
+    if(strcmp(cmd.action.c_str(), "rotl")!=0 && strcmp(cmd.action.c_str(), "rotr")!=0){
+      res.push_back(p);
+    }
+    cur = next;
+  }
+  return res;
+}
+
+std::vector<Pose2D> flat_walk_plan::to_traj(){
+  std::vector<Pose2D> res;
+  res.push_back(Pose2D(init_pose.x, init_pose.y, init_pose.z));
+
+  for(flat_walk_cmd cmd: controls){
+    int num;
+    flat_walk_cmd elem_cmd;
+    std::tie(elem_cmd, num) = flat_walk_cmd::split(cmd);
+    for(size_t n=0; n<num; n++){
+      res.push_back(next_pose(res.back(), elem_cmd));
+    } 
+  }
+  return res;
+}
+
+
+Pose2D flat_walk_plan::next_pose(const Pose2D& ip, const flat_walk_cmd& cmd){
+  cmd_struct cmd_transl= cmd.to_struct();
+  
+  /*std::cout << "Called next pose from " << ip.x << " " << ip.y << " " << ip.yaw << std::endl; 
+  std::cout << "with command " << cmd.action << " " << cmd.amount << std::endl; */
+
+  Pose2D res;
+  res.x = ip.x + cmd_transl.walk_meters*cos(ip.yaw);
+  res.y = ip.y + cmd_transl.walk_meters*sin(ip.yaw);
+  res.yaw = ip.yaw + cmd_transl.turn_deg*M_PI/180;
+  
+  return res;
+}
+
+void flat_walk_plan::from_rrts_unicycle_controls(const float* init_state, std::vector<float*> traj_controls, int seq_num_offset){
+  init_pose = YARP_Point(init_state[0], init_state[1], init_state[2]);
+
+  controls.clear();
+  flat_walk_cmd cmd;
+  double turn2 = 0;
+  int i=0;
+  
+  for(auto c:traj_controls){
+    // To be compliant with interfaces, a
+    // conversion to degrees here is needed
+    
+    c[0] = c[0]*180/M_PI; 
+    c[2] = c[2]*180/M_PI;
+
+    cmd.seq_num = i;
+    i++;
+    cmd.amount = c[0]+turn2;
+    cmd.action = "rotl";
+    cmd.normalize();
+    controls.push_back(cmd);
+    cmd.seq_num = i;
+    i++;
+    cmd.amount = c[1];
+    cmd.action = "fwd";
+    cmd.normalize();
+    controls.push_back(cmd);
+    turn2 = c[2];
+  }
+  cmd.seq_num = i;
+  cmd.amount = turn2;
+  cmd.action = "rotl";
+  cmd.normalize();
+  controls.push_back(cmd);
+}
