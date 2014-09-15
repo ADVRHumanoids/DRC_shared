5455692604eb3ebe836bf269e6883cc2058126f5
Alessandro <ale.settimi@gmail.com>
2014-09-15 19:23:46 +0200

    fs_planner_msg updated

--- include/drc_shared/yarp_msgs/fs_planner_msg.h
+++ include/drc_shared/yarp_msgs/fs_planner_msg.h
@@ -5,13 +5,17 @@
 #include <yarp/os/Portable.h>
 
 #include <yarp/os/Bottle.h>
-
+#include <kdl_parser/kdl_parser.hpp>
 class fs_planner_msg
 {
 public:
     double x,y,z;
     std::string command;
     int num_steps;
+    
+    KDL::Frame left_foot;
+    KDL::Frame right_foot;
+    
     yarp::os::Bottle toBottle()
     {
         yarp::os::Bottle temp;
@@ -28,8 +32,28 @@ public:
         {
             list.addInt(num_steps);
         }
+        if(command=="ik_check" || command=="ik_com_check" || command=="custom_step")
+	{
+	    list.addDouble(left_foot.p.x());
+	    list.addDouble(left_foot.p.y());
+	    list.addDouble(left_foot.p.z());
+	    double ro,pi,ya;
+	    left_foot.M.GetRPY(ro,pi,ya);
+	    list.addDouble(ro);
+	    list.addDouble(pi);
+	    list.addDouble(ya);
+	    
+	    list.addDouble(right_foot.p.x());
+	    list.addDouble(right_foot.p.y());
+	    list.addDouble(right_foot.p.z());
+	    right_foot.M.GetRPY(ro,pi,ya);
+	    list.addDouble(ro);
+	    list.addDouble(pi);
+	    list.addDouble(ya);
+	}
         return temp;
     }
+    
     
     void fromBottle(yarp::os::Bottle* temp)
     {
@@ -62,7 +86,25 @@ public:
         {
             num_steps=list->get(1).asInt();
         }
-
+        if(command=="ik_check" || command=="ik_com_check" || command=="custom_step")
+	{
+	    left_foot.p.x(list->get(1).asDouble());
+	    left_foot.p.y(list->get(2).asDouble());
+	    left_foot.p.z(list->get(3).asDouble());
+	    double ro,pi,ya;
+	    ro = list->get(4).asDouble();
+	    pi = list->get(5).asDouble();
+	    ya = list->get(6).asDouble();
+	    left_foot.M.RPY(ro,pi,ya);
+	    
+	    right_foot.p.x(list->get(7).asDouble());
+	    right_foot.p.y(list->get(8).asDouble());
+	    right_foot.p.z(list->get(9).asDouble());
+	    ro = list->get(10).asDouble();
+	    pi = list->get(11).asDouble();
+	    ya = list->get(12).asDouble();
+	    right_foot.M.RPY(ro,pi,ya);
+	}
 	return;
     }
   
