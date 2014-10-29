88cf1c7b5f5e942a7c749c43437ed13c15c68871
Alessandro <ale.settimi@gmail.com>
2014-10-29 17:22:31 +0100

    fs_perception_msg updated

--- include/drc_shared/yarp_msgs/fs_perception_msg.h
+++ include/drc_shared/yarp_msgs/fs_perception_msg.h
@@ -5,13 +5,35 @@
 #include <yarp/os/Portable.h>
 
 #include <yarp/os/Bottle.h>
+#include <boost/concept_check.hpp>
 #include <kdl/frames.hpp>
 
+class point_2d
+{
+public:
+    double x;
+    double y;
+};
+
+class region
+{
+public:
+    point_2d start;
+    point_2d end;
+};
 
 class fs_perception_msg
 {
 public:
     std::string command;
+    
+    double image_fps;
+    
+    std::string point_option;
+    bool left_image;
+    
+    point_2d point;
+    region rectangle;
 
     yarp::os::Bottle toBottle()
     {
@@ -19,9 +41,24 @@ public:
         yarp::os::Bottle& list= temp.addList();
 
 	list.addString(command);
-	if(command=="send_stereo_cloud")
+
+	if(command=="send_image")
 	{
-	    list.addString(command);
+	    list.addInt(left_image);
+	    list.addDouble(image_fps);
+	}
+	if(command=="focus_on_region")
+	{
+	    list.addDouble(rectangle.start.x);
+	    list.addDouble(rectangle.start.y);
+	    list.addDouble(rectangle.end.x);
+	    list.addDouble(rectangle.end.y);
+	}
+	if(command=="focus_on_point")
+	{
+	    list.addDouble(point.x);
+	    list.addDouble(point.y);
+	    list.addString(point_option);
 	}
         return temp;
     }
@@ -47,8 +84,23 @@ public:
 
         command = list->get(0).asString();
 
-	if(command=="send_stereo_cloud") {
-	    //TODO
+	if(command=="send_image")
+	{
+	    left_image = list->get(1).asInt();
+	    image_fps = list->get(2).asDouble();
+	}
+	if(command=="focus_on_region")
+	{    
+	    rectangle.start.x = list->get(1).asDouble();
+	    rectangle.start.y = list->get(2).asDouble();
+	    rectangle.end.x = list->get(3).asDouble();
+	    rectangle.end.y = list->get(4).asDouble();
+	}
+	if(command=="focus_on_point")
+	{
+	    point.x = list->get(1).asDouble();
+	    point.y = list->get(2).asDouble();
+	    point_option = list->get(3).asString();
 	}
 	return;
     }
