a5b3aea6ed3c00d175afee112acabef6c9458548
corradopav <corradopav@gmail.com>
2015-03-18 13:31:23 +0100

    included drill message

---
+++ include/drc_shared/yarp_msgs/drill_msg.h
@@ -0,0 +1,89 @@
+#ifndef DRILL_MSG
+#define DRILL_MSG
+
+#include <string>
+#include <yarp/os/Portable.h>
+#include <yarp/os/Bottle.h>
+#include <kdl/frames.hpp>
+
+class drill_msg
+{
+public:
+    drill_msg()
+    {
+	// TODO ADD RIGHT CUSTOM DATA
+	command="";
+	frame="";
+	drill_data.p.x(0.0);
+	drill_data.p.y(0.0);
+	drill_data.p.z(0.0);
+	drill_data.M = KDL::Rotation::Identity();
+    }
+  
+    KDL::Frame drill_data;
+    std::string command;  
+    std::string frame;
+
+    yarp::os::Bottle toBottle()
+    {
+        yarp::os::Bottle temp;
+        yarp::os::Bottle& list= temp.addList();
+
+	list.addString(command);
+
+	if(command=="drilldatasent")
+	{
+	    list.addString(frame);
+	    list.addDouble(drill_data.p.x());
+	    list.addDouble(drill_data.p.y());
+	    list.addDouble(drill_data.p.z());
+	    double ro,pi,ya;
+	    drill_data.M.GetRPY(ro,pi,ya);
+	    list.addDouble(ro);
+	    list.addDouble(pi);
+	    list.addDouble(ya);
+	}
+	
+        return temp;
+    }
+
+    void fromBottle(yarp::os::Bottle* temp)
+    {
+        if (temp->get(0).isNull())
+        {
+            command="";
+            return;
+        }
+        yarp::os::Bottle* list = temp->get(0).asList();
+        if (list==NULL)
+        {
+            command="";
+            return;
+        }
+        if (list->get(0).isNull())
+        {
+            command="";
+            return;
+        }
+
+        command = list->get(0).asString();
+
+	if(command=="drilldatasent")
+	{
+	    frame = list->get(1).asString();
+	    drill_data.p.x(list->get(2).asDouble());
+	    drill_data.p.y(list->get(3).asDouble());
+	    drill_data.p.z(list->get(4).asDouble());
+	    double ro,pi,ya;
+	    ro = list->get(5).asDouble();
+	    pi = list->get(6).asDouble();
+	    ya = list->get(7).asDouble();
+	    drill_data.M = KDL::Rotation::RPY(ro,pi,ya);
+	}
+
+	return;
+    }
+  
+};
+
+#endif // DOOR_MSG
