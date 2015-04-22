ab698ae967b9f0f4bffef0ab1319c5e1af8621fe
Alessandro <ale.settimi@gmail.com>
2015-04-22 19:53:16 +0200

    added drc_door and drc_wall states in all_definitions.h

--- include/drc_shared/module_statuses/all_definitions.h
+++ include/drc_shared/module_statuses/all_definitions.h
@@ -2,6 +2,8 @@
 #define ALL_DEFINITION_H
 
 #include "drc_shared/module_statuses/drc_poses_statuses.h"
+#include "drc_shared/module_statuses/drc_door_statuses.h"
+#include "drc_shared/module_statuses/drc_wall_statuses.h"
 
 namespace walkman
 {
@@ -15,6 +17,14 @@ public:
         poses::status_definitions poses_states;
 	module_code_to_status["drc_poses"] = poses_states.code_to_status;
 	module_status_to_code["drc_poses"] = poses_states.status_to_code;
+
+	door::status_definitions door_states;
+	module_code_to_status["drc_door"] = door_states.code_to_status;
+	module_status_to_code["drc_door"] = door_states.status_to_code;
+
+	wall::status_definitions wall_states;
+	module_code_to_status["drc_wall"] = wall_states.code_to_status;
+	module_status_to_code["drc_wall"] = wall_states.status_to_code;
     }
     
     std::map<std::string, std::map<std::string,std::string>> module_code_to_status;
