8a2ef704537fd10864b58f6c5290d552720cc409
Alessandro <ale.settimi@gmail.com>
2015-04-22 19:34:07 +0200

    added drc_door_statuses.h

---
+++ include/drc_shared/module_statuses/drc_door_statuses.h
@@ -0,0 +1,60 @@
+#ifndef DRC_DOOR_STATUSES_H
+#define DRC_DOOR_STATUSES_H
+
+#include <map>
+#include <vector>
+
+#define HOMING_STATUS "homing"
+#define STAND_STATUS "stand"
+#define HOMING_GUARD_STATUS "homing_guard"
+
+namespace walkman
+{
+namespace drc
+{
+namespace door
+{
+class status_definitions
+{
+public:
+    status_definitions()
+    {
+        std::vector<std::string> states;
+
+	states.push_back("idle");
+	states.push_back("ready");
+	states.push_back("prereaching");
+	states.push_back("prereached");
+	states.push_back("reaching");
+	states.push_back("reached");
+	states.push_back("approaching");
+	states.push_back("approached");
+	states.push_back("grasping");
+	states.push_back("grasped");
+	states.push_back("turning");
+	states.push_back("turned");
+	states.push_back("opening_door");
+	states.push_back("opened_door");
+	states.push_back("supporting hand");
+	states.push_back("supported hand");
+	states.push_back("leaving handle");
+	states.push_back("left handle");
+	states.push_back("safe_exiting");
+	states.push_back("safe_exited");
+
+	char code='a';
+	for(auto state:states)
+	{
+	    status_to_code[state]=std::string(1,code);
+	    code_to_status[std::string(1,code)]=state;
+	    code = (char)(code+1);
+	}
+    }
+    std::map<std::string,std::string> status_to_code;
+    std::map<std::string,std::string> code_to_status;
+};
+}
+}
+}
+
+#endif //DRC_DOOR_STATUSES_H
