53bfb6dfd0d5150bfd9b811b75a35627fdef77e7
Enrico Corvaglia <enrico988@gmail.com>
2015-04-27 19:18:01 +0200

    fixed statuses

--- include/drc_shared/module_statuses/drc_door_statuses.h
+++ include/drc_shared/module_statuses/drc_door_statuses.h
@@ -29,16 +29,18 @@ public:
 	states.push_back("grasped");
 	states.push_back("turning");
 	states.push_back("turned");
-	states.push_back("opening_door");
-	states.push_back("opened_door");
+	states.push_back("opening door");
+	states.push_back("opened door");
 	states.push_back("supporting hand");
 	states.push_back("supported hand");
 	states.push_back("ungrasping");
 	states.push_back("ungrasped");
 	states.push_back("leaving handle");
 	states.push_back("left handle");
-	states.push_back("safe_exiting");
-	states.push_back("safe_exited");
+        states.push_back("more opening");
+        states.push_back("more opened");
+	states.push_back("safe exiting");
+	states.push_back("safe exited");
 
 	char code='a';
 	for(auto state:states)
