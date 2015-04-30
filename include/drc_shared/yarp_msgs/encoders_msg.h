326074027c18caa7f4e2ff21339c24571556151d
Alberto Cardellino <alberto.cardellino@iit.it>
2015-04-30 19:05:29 +0200

    Add torque message

--- include/drc_shared/yarp_msgs/encoders_msg.h
+++ include/drc_shared/yarp_msgs/encoders_msg.h
@@ -20,7 +20,7 @@ class encoder_msg
 private:
 public:
     std::vector<double> encoders;
-
+    std::vector<double> torques;
     encoder_msg()
     {
     }
@@ -28,9 +28,13 @@ public:
     yarp::os::Bottle toBottle()
     {
         yarp::os::Bottle temp;
-//         temp.addInt(encoders.size());
-        char buffer[300];
+        int n_joints = encoders.size();
+        char buffer[500];
         int size=0;
+        buffer[size] = (uint8_t) n_joints;
+        size++;
+
+        // add encoders
         for (int i=0;i<encoders.size();i++)
         {
             encoder_to_int a;
@@ -40,11 +44,20 @@ public:
             buffer[size]=a.chars[1];
             size++;
         }
+
+        // add torques
+        for (int i=0;i<n_joints;i++)
+        {
+            encoder_to_int a;
+            buffer[size]= (uint8_t) round((torques[i] + 200)/2);
+            size++;
+        }
+
         yarp::os::Value a(buffer,size);
         temp.add(a);
         return temp;
     }
-    
+
     void fromBottle(yarp::os::Bottle* temp)
     {
         int getter=0;
@@ -56,24 +69,32 @@ public:
 //         assert(size*2==temp->get(getter).asBlobLength());
         int size = temp->get(getter).asBlobLength();
         if (size==0) return;
-        encoders.resize(size/2);
-        char buffer[300];
+
+        char buffer[500];
         const char* source = temp->get(getter).asBlob();getter++;
         for (int i=0;i<size;i++)
         {
             buffer[i]=source[i];
         }
+
+        uint8_t n_joints = buffer[0];
+        encoders.resize(n_joints);
         int count = 0;
-        for (int i=0;i<size;i=i+2)
+        for (uint8_t i=0; i<n_joints; i++)
         {
             encoder_to_int enc;
             enc.chars[0]=buffer[count];
             count++;
             enc.chars[1]=buffer[count];
             count++;
-            encoders[i/2]=enc.enc/50.0;
+            encoders[i]=enc.enc/50.0;
         }
 
+        for (uint8_t i=0; i<n_joints, count<size-1; i++)
+        {
+            torques[i] = (double) (buffer[count] * 2) - 200;
+            count++;
+        }
         return;
     }
 
