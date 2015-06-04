f3536b5d63e3c7f4b31682fd992a5ed5c9bcaf34
corradopav <corradopav@gmail.com>
2015-06-04 10:14:06 -0700

    updated drive statuses

--- include/drc_shared/module_statuses/drc_drive_statuses.h
+++ include/drc_shared/module_statuses/drc_drive_statuses.h
@@ -16,14 +16,27 @@ public:
     status_definitions()
     {
         std::vector<std::string> states;
-
-	states.push_back("idle");
-	states.push_back("ready");
-	states.push_back("data_received");
-	states.push_back("aligning_hand");
-	states.push_back("turning_left");
-	states.push_back("turning_right");
-	states.push_back("accelerating");
+		
+	states.push_back("idle");//"idle";
+	states.push_back("ready");//"ready";
+	states.push_back("reaching");//"reaching";
+	states.push_back("reached");//"reached";
+	states.push_back("approaching");//"approaching";
+	states.push_back("approached");//"approached";
+	states.push_back("rotating_foot");//"rotating_foot";
+	states.push_back("rotated_foot");//"rotated_foot";
+	states.push_back("drive");//"drive";
+	states.push_back("turning_left");//"turning_left";
+	states.push_back("turning_right");//"turning_right";
+	states.push_back("accelerating");//"accelerating";
+	states.push_back("moving_away_hand");//"moving_away_hand";
+	states.push_back("moved_away_hand");//"moved_away_hand";
+	states.push_back("grasping");//"grasping";
+	states.push_back("grasped");//"grasped";
+	states.push_back("ungrasping");//"ungrasping";
+	states.push_back("ungrasped");//"ungrasped";
+	states.push_back("moving_away_foot");//"moving_away_foot";
+	states.push_back("moved_away_foot");//"moved_away_foot";
 
 	char code='a';
 	for(auto state:states)
