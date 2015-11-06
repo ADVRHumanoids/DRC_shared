b86ea05e2133f68d1939ee72c14a74e7ab8b2cde
Alessio Rocchi <alessio.rocchi@iit.it>
2015-11-06 11:36:16 +0100

    Updated license headers

--- src/planner_msgs/flat_walk_cmd.cpp
+++ src/planner_msgs/flat_walk_cmd.cpp
@@ -1,4 +1,4 @@
-/* Copyright [2014,2015] [Mirko Ferrati, Alessandro Settimi, Valerio Varricchio]
+/* Copyright [2014,2015] [Alessio Rocchi, Mirko Ferrati, Alessandro Settimi, Valerio Varricchio]
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
