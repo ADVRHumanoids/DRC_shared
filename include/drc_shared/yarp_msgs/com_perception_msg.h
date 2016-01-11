d13b995085c6848e5871ffd2fc7bf747edcb52a0
Dimitrios Kanoulas <dimitrios.kanoulas@iit.it>
2016-01-11 17:41:12 +0100

    Tweaks

--- include/drc_shared/yarp_msgs/com_perception_msg.h
+++ include/drc_shared/yarp_msgs/com_perception_msg.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* Software License Agreement (BSD License)
  *
  *  Copyright (c) 2016-, Dimitrios Kanoulas
@@ -32,21 +31,6 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  */
-=======
-/* Copyright [2014,2015] [Mirko Ferrati, Alessandro Settimi, Valerio Varricchio]
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.*/
->>>>>>> b86ea05e2133f68d1939ee72c14a74e7ab8b2cde
 
 #ifndef COM_PERCEPTION_MSG
 #define COM_PERCEPTION_MSG
